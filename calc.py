733225150c96ad820ccd944e3145bed2f62157a6
tykm <tylerkim01@gmail.com>
2022-10-10 22:25:28 -0400

    builds category list

--- calc.py
+++ calc.py
@@ -1,19 +1,23 @@
 import csv
 
+categories = set()
 
 with open('amex2022.csv', 'r') as csv_file:
     reader = csv.reader(csv_file)
-
     i = 0
     for row in reader:
-        ct = 0
-        for item in row:
-            ct += 1
-            #print(item)
-        #print('\n')
-        if ct != 11:
-            print(row)
+        cat = row[10]
+        if cat not in categories:
+            categories.add(cat)
+
+
+
+
         # breaks loop after testing first row
         #if i == 1:
         #    break
         #i += 1
+    categories.remove('Category')
+    print(len(categories))
+    for item in categories:
+        print(item)
