328ab606ba878b29c860a668e622e562242eb370
Tyler Kim <tykm@Tylers-MacBook-Pro.local>
2022-10-10 21:35:43 -0400

    verifying amex csv data has clean arrays

---
+++ calc.py
@@ -0,0 +1,18 @@
+import csv
+
+with open('amex2022.csv', 'r') as csv_file:
+    reader = csv.reader(csv_file)
+
+    i = 0
+    for row in reader:
+        ct = 0
+        for item in row:
+            ct += 1
+            #print(item)
+        #print('\n')
+        if ct != 11:
+            print(row)
+        # breaks loop after testing first row
+        #if i == 1:
+        #    break
+        #i += 1
